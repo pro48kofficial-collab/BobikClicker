bbe0460acd9299f3bf4e56867607340c97d4a1cf
pro48kofficial-collab <pro48k.official@gmail.com>
2026-08-22 14:58:19 +0300

    Bind server to all network interfaces

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ setInterval(() => {
 }, 1000);
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
+http.listen(PORT, '0.0.0.0', () => {
     console.log(`Сервер успішно запущено на порту ${PORT}`);
 });
+                                     
